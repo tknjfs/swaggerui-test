7a61825c1eba6408a50668dc1d20faf0c23d95bd
tknjfs <50839411+tknjfs@users.noreply.github.com>
2024-04-25 17:00:15 +0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://raw.githubusercontent.com/tknjfs/Eclipse-Charts/main/openapi.yaml?token=GHSAT0AAAAAACP2IGT3XW7T4YYDR3KKQWHQZRKMIHA",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
